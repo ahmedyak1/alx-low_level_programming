1ad2e5dd606351a1269dcfc314f4127cb3ff9296
ahmed <ahmedyakine1@gmail.com>
2023-08-01 12:54:37 -0700

    task 0

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,6 +3,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h> 
+/**
+ * struct listint_s - More singly linked lists
+ * @nbr: integer
+ * @next: pointer of the next node
+ *
+ */
 typedef struct listint_s
 {int nbr;struct listint_s *next;}
   listint_t;
