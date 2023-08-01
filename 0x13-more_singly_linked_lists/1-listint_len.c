6b698336a08dcf954691696d39d39443ecf2fc1d
ahmed <ahmedyakine1@gmail.com>
2023-08-01 15:21:37 -0700

    task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -4,14 +4,15 @@
 /** task 1 length list
   * listint_len - obtains the number of nodes in a listint_t list
   * @h: single list to print
+  *
   * Return: Count of nodes
   */
-size_t
-listint_len (const listint_t * h)
+size_t listint_len (const listint_t *h)
 {
   size_t j;
 
-  for (j = 0; h; j++)
-    h = h->next;
+    for (j = 0; h; j++)
+        h = h->next;
+ 
   return (j);
 }
