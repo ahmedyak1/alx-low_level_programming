3bd826740b66abb976444366ff386aa0cca9e74f
ahmed <ahmedyakine1@gmail.com>
2023-07-31 17:19:55 -0700

    task 0

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,5 +1,5 @@
-#ifndef object_like_macro
-#define object_like_macro
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
 // Object-Like MACRO SIZE for the token 1024
 #define SIZE 1024
 #endif
