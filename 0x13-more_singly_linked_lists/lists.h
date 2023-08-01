9db5e56a3b83be9b254e6b94f26a3ebf4c0b7975
ahmed <ahmedyakine1@gmail.com>
2023-08-01 16:52:57 -0700

    task 2

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,8 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
-
+#include <stddef.h>
+#include <stdlib.h>
 #include <string.h>
-#include <stdio.h> 
+#include <stdio.h>
 
 /**
  * struct listint_s - More singly linked lists
