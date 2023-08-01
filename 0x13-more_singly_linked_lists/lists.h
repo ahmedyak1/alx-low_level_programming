0dd90d83ac29907eaba286c0f914bff4d50e176e
ahmed <ahmedyakine1@gmail.com>
2023-08-01 13:03:41 -0700

    task 0

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,3 +1,4 @@
+#ifndef LISTS_H
 #define LISTS_H
 #include <stddef.h>
 #include <stdio.h>
@@ -10,5 +11,10 @@
  *
  */
 typedef struct listint_s
-{int nbr;struct listint_s *next;}
+{int n;struct listint_s *next;}
   listint_t;
+/**
+ * Task 0 print_listint
+ */
+size_t print_listint(const listint_t *h);
+#endif
