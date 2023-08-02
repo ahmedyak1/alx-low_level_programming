dd20bad11a8f62a25a56a3437ebe234f085e60f9
ahmed <ahmedyakine1@gmail.com>
2023-08-02 12:02:52 -0700

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,18 +15,18 @@ struct dog
   char *owner;
 };
 typedef struct dog;
-/* Task 0 define struct dog*/
-/* Task 3 dog_t */
+/*** Task 0 define struct dog*/
+/*** Task 3 dog_t */
 
-/* Task 1 init_dog*/
+/*** Task 1 init_dog*/
 void init_dog (struct dog *d, char *name, float age, char *owner);
-/* Task 2 print_dog*/
+/*** Task 2 print_dog*/
 void print_dog (struct dog *d);
-/* Task 4 dog_t new_dog*/
+/*** Task 4 dog_t new_dog*/
 dog_t *new_dog (char *name, float age, char *owner);
 int _strlen (char *s);
 char *_strcpy (char *dest, char *src);
-/* Task 5 free_dog*/
+/*** Task 5 free_dog*/
 void free_dog (dog_t * d);
 
 
