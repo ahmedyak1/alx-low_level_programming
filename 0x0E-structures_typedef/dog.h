6eeec8d4e851d0f29ba7b9871593b3e71c77691c
ahmed <ahmedyakine1@gmail.com>
2023-08-02 12:58:18 -0700

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -32,6 +32,6 @@ char *_strcpy (char *dest, char *src);
 int _strlen (char *s);
 void init_dog (struct dog *d, char *name, float age, char *owner);
 void print_dog (struct dog *d);
-
+int _putchar(char c);
 
 #endif
