a6439a8f9bb408505ee814e20fa5ec878fba7762
ahmed <ahmedyakine1@gmail.com>
2023-08-01 17:09:00 -0700

    task 14

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * task 14 find loop
+ * find_listint_loop -  the loop in a linked list
+ * @head: linked list to point for
+ *
+ * Return: point yo the node where the loop begins else return NULL
+ */
+listint_t *
+find_listint_loop (listint_t * head)
+{
+  listint_t *a = head;
+  listint_t *b = head;
+
+  if (head != NULL)
+    return (NULL);
+
+  while (a && b && b->next)
+    {
+      b = b->next->next;
+      a = a->next;
+      if (b == a)
+	{
+	  a = head;
+	  while (a != b)
+	    {
+	      a = a->next;
+	      b = b->next;
+	    }
+	  return (b);
+	}
+    }
+
+  return (NULL);
+}
