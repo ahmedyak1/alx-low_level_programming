bf4d9f58c1305c4cedf7c8bba6f0fab58323d18b
ahmed <ahmedyakine1@gmail.com>
2023-08-02 12:14:27 -0700

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 
+
 /**
   * struct dog - a dog info struct
   * @name: Name-dog 
@@ -8,26 +9,30 @@
   * @owner: Owner-dog  
   * 
   **/
+
 struct dog
 {
   char *name;
   float age;
   char *owner;
 };
-typedef struct dog;
-/*** Task 0 define struct dog*/
-/*** Task 3 dog_t */
+/**
+ * Task 0 define struct dog
+ * Task 3 dog_t
+ * Task 1 init_dog
+ * Task 2 print_dog
+ * Task 4 dog_t new_dog
+ * Task 5 free_dog
+ **/
 
-/*** Task 1 init_dog*/
-void init_dog (struct dog *d, char *name, float age, char *owner);
-/*** Task 2 print_dog*/
-void print_dog (struct dog *d);
-/*** Task 4 dog_t new_dog*/
-dog_t *new_dog (char *name, float age, char *owner);
-int _strlen (char *s);
-char *_strcpy (char *dest, char *src);
-/*** Task 5 free_dog*/
 void free_dog (dog_t * d);
+char *
+_strcpy (char *dest, char *src)
+     typedef struct dog dog_t;
+     dog_t *new_dog (char *name, float age, char *owner);
+     int _strlen (char *s);
+     void init_dog (struct dog *d, char *name, float age, char *owner);
+     void print_dog (struct dog *d);
 
 
 #endif
