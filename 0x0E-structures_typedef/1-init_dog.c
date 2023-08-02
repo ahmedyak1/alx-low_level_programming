2e88a049aa9dbb93a930c8a1ea37bbe4dda47e8e
ahmed <ahmedyakine1@gmail.com>
2023-08-02 12:37:02 -0700

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "dog.h"
+
+
+/**
+  * Task 1 - Init dog 
+  * init_dog - declare a variable of type struct dog
+  * @d: point to struct dog to declare
+  * @name: name to declare
+  * @age: age to declare
+  * @owner: owner to declare
+  * 
+  * 
+  * Return : nothing
+**/
+void
+init_dog (struct dog *d, char *name, float age, char *owner)
+{
+  if (doggy != NULL)
+    {
+      doggy = malloc (sizeof (struct dog));
+
+      doggy->name = name;
+
+      doggy->age = age;
+
+      doggy->owner = owner;
+    }
+}
