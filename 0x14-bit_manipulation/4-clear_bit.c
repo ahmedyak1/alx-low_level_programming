7c3dec81074d1f3e15ef17d5a6a9e9537ae2e7a0
ahmed <ahmedyakine1@gmail.com>
2023-08-03 17:39:34 -0700

    task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * clear_bit - sets the value of a given bit to 0
+ * @n: pointe number 
+ * @index: position of the bit to clear
+ *
+ * 
+ * Return: 1 for succcced else for failure
+ */
+int
+clear_bit (unsigned long int *n, unsigned int index)
+{
+  if (index > 63)
+    {
+      return (-1);
+    }
+
+  *n = (~(1UL << index) & *n);
+  return (1);
+}
+
