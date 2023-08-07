2093cc306e410da1d08a3452c7b5d5d507cbfd43
ahmed <ahmedyakine1@gmail.com>
2023-08-07 16:16:56 -0700

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * create_file - Make a file.
+ * 
+ * 
+ * @filename: To point the name of file to make.
+ * @text_content: To point a string the out of the file.
+ *
+ * Return: Return -1 fail else return 1
+ *         
+ */
+int
+create_file (const char *filename, char *text_content)
+{
+  int filed;
+  int m;
+  int k = 0;
+
+  if (filename == NULL)
+    return (-1);
+
+  if (text_content != NULL)
+    {
+      for (k = 0; text_content[k];)
+	k++;
+    }
+
+  filed = open (filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
+  m = write (filed, text_content, k);
+
+  if (filed == -1 || m == -1)
+    {
+      return (-1);
+    }
+
+  close (filed);
+
+  return (1);
+}
+
