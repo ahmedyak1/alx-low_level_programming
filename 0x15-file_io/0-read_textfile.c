d4a704895dfbe067bb027ce85a72d7e76672b1cc
ahmed <ahmedyakine1@gmail.com>
2023-08-07 16:06:41 -0700

    task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile- Read string file print to stdout.
+ * 
+ * 
+ * @filename: text file to read
+ * @letters: Count of letters to read
+ * Return: Count of read and prtint return 0 failed
+ *       
+ */
+ssize_t
+read_textfile (const char *filename, size_t letters)
+{
+  char *bu;
+  ssize_t filed;
+  ssize_t x;
+  ssize_t y;
+
+  filed = open (filename, O_RDONLY);
+  
+  if (filed == -1)
+    return (0);
+    
+  bu = malloc (sizeof (char) * letters);
+  y = read (filed, bu, letters);
+  x = write (STDOUT_FILENO, bu, y);
+
+  free (bu);
+  close (filed);
+  return (x);
+  
+  
+}
+
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,13 @@
+# 0x15. C - File I/O
+
+---
+#### Task 0 - 0. Tread lightly, she is near
+_Write a function that reads a text file and prints it to the POSIX standard output._
+#### Task 1 - 1. Under the snow
+_Create a function that creates a file._
+#### Task 2 - 2. Speak gently, she can hear
+_Write a function that appends text at the end of a file._
+#### Task 3 - 3. 11
+_Write a function that sets the value of a bit to 1 at a given index._
+#### Task 4 - 3. cp
+_Write a program that copies the content of a file to another file._
