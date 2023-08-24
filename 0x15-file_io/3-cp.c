05165b449e4a3461df984d8c58e925136f5e1384
ahmedyak1 <ahmedyakine1@gmail.com>
2023-08-24 16:05:07 -0700

    task 4

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,15 +15,13 @@ error_f (int file_from, int file_to, char *argv[])
 {
   if (file_to == -1)
     {
-      dprintf (STDERR_FILENO, "Error: Unable to perform writing to %s\n",
-	       argv[2]);
+      dprintf (STDERR_FILENO, "Error: Can't write to %s\n",argv[2]);
 
       exit (99);
     }
   if (file_from == -1)
     {
-      dprintf (STDERR_FILENO,
-	       "Error: Reading from the file is not possible %s\n", argv[1]);
+      dprintf (STDERR_FILENO,"Error: Can't read from file %s\n", argv[1]);
 
       exit (98);
     }
@@ -84,4 +82,3 @@ main (int argc, char *argv[])
     }
   return (0);
 }
-
