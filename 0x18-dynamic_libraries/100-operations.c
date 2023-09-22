4edfdcb122fcfabd2c4811af18a9792ad6a1e52d
ahmedyak1 <ahmedyakine1@gmail.com>
2023-09-22 16:51:15 -0700

    task 2.0

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,25 @@
+int add(int a, int b)
+{
+   return (b + a);
+}
+
+int sub(int a, int b)
+{
+   return (-b + a);
+}
+
+int mul(int a, int b)
+{
+    return (b * a);
+}
+
+int div(int a, int b)
+{
+    return (a / b);
+}
+
+int mod(int a, int b)
+{
+    return (a % b);
+}
+
