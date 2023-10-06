8bb1e22d40d76c0d0f56a54f305dd661ec490477
ahmedyak1 <ahmedyakine1@gmail.com>
2023-10-06 16:29:25 -0700

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,10 +1,13 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set -  function add new node hash table 
+ * hash_table_set -  function that adds an element to the hash table.
+ * @ht: index pointer to hash table 
+ * @key: index pointer to key 
+ * @value: index pointer to the value 
  *
  *
- * Return: 1 succec else 0
+ * Return: retunr 0 failed otherwise 1
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
@@ -12,9 +15,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *cn = NULL;
 	unsigned long int k;
 
-	if (ht == NULL || strcmp(key, "") == 0 || key == NULL || value == NULL )
+	if (ht == NULL || key == NULL || value == NULL || strcmp(key, "") == 0)
 		return (0);
-	
 	k = key_index((unsigned char *)key, ht->size);
 	cn = ht->array[k];
 
@@ -32,7 +34,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 
 	nn = _create_node_(key, value);
-	if (nn)
+	if (nn == NULL)
 		return (0);
 	nn->next = ht->array[k];
 	ht->array[k] = nn;
@@ -41,23 +43,27 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 
 /**
- * _create_node_ - function that creates a new_node node.
+ * _node_ - function new_node node
+ * @key: pointer to key.
+ * @value: pointe
+ *
+ * Return: pointer to nn node otherwise NULL
  */
 hash_node_t *_create_node_(const char *key, const char *value)
 {
 	hash_node_t *nn;
 
-	if (key == NULL ||  value == NULL || strcmp(key, "") == 0 )
+	if (key == NULL || strcmp(key, "") == 0 || value == NULL)
 		return (NULL);
 
 	nn = malloc(sizeof(hash_node_t));
 
-	if (!nn)
+	if (nn == NULL)
 		return (NULL);
 
 	nn->key = strdup(key);
 
-	if (!(nn->key))
+	if (nn->key == NULL)
 	{
 		free(nn);
 		return (NULL);
@@ -65,7 +71,7 @@ hash_node_t *_create_node_(const char *key, const char *value)
 
 	nn->value = strdup(value);
 
-	if (!(nn->value))
+	if (nn->value == NULL)
 	{
 		free(nn->key);
 		free(nn);
@@ -73,7 +79,6 @@ hash_node_t *_create_node_(const char *key, const char *value)
 	}
 
 	nn->next = NULL;
-	
 	return (nn);
 }
 
